7ff96c446682909398b449dc0f02c6b55cafaf02
amond <amond@amond.net>
2014-05-30 13:54:42 +0900

    first snapshot
    ok

--- js/printTitle.js
+++ js/printTitle.js
@@ -1,34 +1,20 @@
+$(document).ready(function() {
 
-var count = 0;
+    $("#owl-demo").owlCarousel({
 
-function printTitle(){
-	var titleList = new Array();
-	titleList[0] = '1.아직도... 평일 낮 시간에 골프장이 텅텅 비어있나요?';
-	titleList[1] = '2. 아직도... 스미싱에 떨고 있는 고객에게 비효율적인 문자광고를 하시나요?';
-	titleList[2] = '3. 아직도... 골프장을 방문했던 모든 고객들의 정보를 얻지 못하고 있나요?';
-	titleList[3] = '2. 아직도... 스미싱에 떨고 있는 고객에게 비효율적인 <br/>문자광고를 하시나요?';
-	titleList[4] = '3. 아직도... 골프장을 방문했던 모든 고객들의 정보를 <br/>얻지 못하고 있나요?';
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+        autoPlay: true
 
-	var title = document.getElementById("intro");
-	
-	switch(count){
-		case 0:
-			title.innerHTML = '<h2 id=titlelist>' + titleList[0] + '</h2>' + '<h4>' + titleList[1] + '</h4>' + '<h4>' + titleList[2] + '</h4>';
-			break;
-		case 1:
-			title.innerHTML = '<h4>' + titleList[0] + '</h4>' + '<h2 id=titlelist>' + titleList[3] + '</h2>' + '<h4>' + titleList[2] + '</h4>';
-			break;
-		default:
-			title.innerHTML = '<h4>' + titleList[0] + '</h4>' + '<h4>' + titleList[1] + '</h4>' + '<h2 id=titlelist>' + titleList[4] + '</h2>';
-			break;
-	}
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
 
-	if(count < 2) count++;
-	else count=0;
+    });
 
-	setTimeout("printTitle()", 6900);
-}
-
-window.onload = function(){
-	printTitle();
-}
+});
